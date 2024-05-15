2892d7a30e657dd44e5649c44966d95e65ef8841
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-05-15 16:15:56 +0000

    DCIT308 assignment 1: Write a program to implement the creation story of Genesis chapter 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         CreationStory creationStory = new CreationStory();
         creationStory.getStories();
-        System.out.println(creationStory.getStoryAtIndex(10));
+        System.out.println("Enter the day you want to know");
+        int day = scanner.nextInt();
+        System.out.println(creationStory.getStoryAtIndex(day));
     }
 }
