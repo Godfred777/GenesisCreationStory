b88f737956bb650419d07da57024cf6556a6228b
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-05-15 16:00:21 +0000

    DCIT308 assignment 1: Write a program to implement the creation story of Genesis chapter 1

---
+++ src/CreationStory.java
@@ -0,0 +1,24 @@
+public class CreationStory {
+    private String[] stories = {
+            "Created Heavens and the Earth in the Beginning",
+            "Brought light to the Earth from the Heavens",
+            "Created the atmosphere by separating water between the earth and heavens",
+            "Created the land, sea and vegetation",
+            "Revealed the sun, moon and stars",
+            "Created sea creatures and birds",
+            "Created land animals and Humans",
+            "Rested on this day"
+
+    };
+
+    public void getStories() {
+        int day = 0;
+        String story = "";
+        while (day < stories.length) {
+            story = stories[day];
+            System.out.println(story);
+            day ++;
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CreationStory creationStory = new CreationStory();
+        creationStory.getStories();
+    }
+}
